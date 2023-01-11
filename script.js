adc5f63dcdec959d3bab35b2faf67aa5e18a7719
Nikita <nikita@oden.com>
2023-01-12 01:10:30 +0300

    Make the book-module-container openable

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const addBookBtn = document.querySelector('#add-book');
+const bookModuleContainer = document.querySelector('.book-module-container');
+const overlay = document.querySelector('.overlay');
+
+addBookBtn.addEventListener('click', toggleBookModal);
+overlay.addEventListener('mousedown', toggleBookModal);
+
 let userLibrary = [];
 
-function Book() {
-
-}
+function Book() {}
 
 function addBookToLibrary() {
-    
+}
+
+function toggleBookModal() {
+  bookModuleContainer.classList.toggle('active');
+  overlay.classList.toggle('active');
 }
