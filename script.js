1fdfd4b626bdab35da1df517b7023dcabfa7cac0
Nikita <nikita@oden.com>
2023-01-12 02:04:32 +0300

    Make so the user can add data to the userLibrary

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-const addBookBtn = document.querySelector('#add-book');
-const bookModuleContainer = document.querySelector('.book-module-container');
-const overlay = document.querySelector('.overlay');
+const addBookBtn = document.querySelector(".add-book");
+const bookModuleContainer = document.querySelector(".book-module-container");
+const overlay = document.querySelector(".overlay");
+const bookForm = document.querySelector('.book-form');
+const submit = document.querySelector(".submit");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
 
-addBookBtn.addEventListener('click', toggleBookModal);
-overlay.addEventListener('mousedown', toggleBookModal);
+const userLibrary = [];
 
-let userLibrary = [];
-
-function Book() {}
-
-function addBookToLibrary() {
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function toggleBookModal() {
-  bookModuleContainer.classList.toggle('active');
-  overlay.classList.toggle('active');
+function toggleBookModule() {
+  bookModuleContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
+
+function retrieveFormData() {
+  return new Book(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readInput.value,
+  );
+}
+
+function addBookToLibrary(e) {
+  userLibrary.push(retrieveFormData());
+  toggleBookModule();
+  bookForm.reset();
+  e.preventDefault();
+}
+
+addBookBtn.addEventListener("click", toggleBookModule);
+overlay.addEventListener("click", toggleBookModule);
+submit.addEventListener("click", addBookToLibrary);
